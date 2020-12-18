e2a13e4f135e2ad8ef13fa67589fd5f1dbe8ac87
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2020-12-17 20:07:21 -0400

    Primer commit

---
+++ app/src/main/java/cl/dyi/app_fragments/MainActivity.java
@@ -0,0 +1,20 @@
+package cl.dyi.app_fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements FragUno.OnFragmentInteractionListener, FragDos.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
